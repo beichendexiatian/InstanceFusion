2cc6fb245438a9e9845f4640a9360bc5c868405e
Jun Yang <2820031394@qq.com>
2019-09-26 17:41:00 +0800

    First submission

---
+++ deps/flann-1.8.4/build/src/cpp/CMakeFiles/flann_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flann_cuda.dir/empty.cpp.o"
+  "../../lib/libflann_cuda.pdb"
+  "../../lib/libflann_cuda.so"
+  "../../lib/libflann_cuda.so.1.8.4"
+  "../../lib/libflann_cuda.so.1.8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flann_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ deps/flann-1.8.4/build/src/cpp/CMakeFiles/flann_cuda_s.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flann_cuda_s.dir/flann/algorithms/flann_cuda_s_generated_kdtree_cuda_3d_index.cu.o"
+  "../../lib/libflann_cuda_s.pdb"
+  "../../lib/libflann_cuda_s.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flann_cuda_s.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
